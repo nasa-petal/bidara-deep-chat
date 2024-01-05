f692416a23adc10179dc3a56ba94d9c5da9a2a3b
Brandon Ruffridge <bruffridge@users.noreply.github.com>
2024-01-05 13:48:54 -0500

    back to square one.

--- App.svelte
+++ App.svelte
@@ -91,8 +91,7 @@
       initialMessages={initialMessages}
       chatStyle={{
         width: "100%",
-        height: "100vh",
-        position: "fixed",
+        height: "100dvh",
         backgroundColor: "white",
         border: "none",
         fontSize: "16px",
@@ -114,9 +113,6 @@
     main {
       font-family: system-ui, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
       display: grid;
-      position:fixed;
-      height:100vh;
-      width:100%;
     }
   </style>
 
--- public/bundle.js
+++ public/bundle.js
@@ -13003,8 +13003,7 @@ var app = (function () {
 
     			set_custom_element_data(deep_chat, "chatstyle", deep_chat_chatstyle_value = {
     				width: "100%",
-    				height: "100vh",
-    				position: "fixed",
+    				height: "100dvh",
     				backgroundColor: "white",
     				border: "none",
     				fontSize: "16px",
@@ -13016,7 +13015,7 @@ var app = (function () {
     			});
 
     			add_location(deep_chat, file, 65, 4, 3599);
-    			attr_dev(main, "class", "svelte-1sqqzqu");
+    			attr_dev(main, "class", "svelte-u93l4o");
     			add_location(main, file, 33, 2, 1851);
     		},
     		l: function claim(nodes) {
