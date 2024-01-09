18f1a5e1f8e25186dd1dc0bf9b6ae8fe7fb9de3f
Brandon Ruffridge <bruffridge@users.noreply.github.com>
2024-01-09 12:47:29 -0500

    updated npm packages to latest versions.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,32 @@
-// this file will not afect the sandbox but will
-// afect the deployment and dowload
-
 import svelte from "rollup-plugin-svelte";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import css from 'rollup-plugin-css-only';
+import livereload from 'rollup-plugin-livereload';
 
 const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
 
 export default {
   input: "index.js",
@@ -18,25 +38,38 @@ export default {
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
-      css: css => {
-        css.write("public/bundle.css");
-      }
+      compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			}
     }),
-
+    // we'll extract any component CSS out into
+    // a separate file — better for performance
+    css({ output: 'bundle.css' }),
     // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration —
-    // consult the documentation for details:
-    // https://github.com/rollup/rollup-plugin-commonjs
-    resolve(),
-    commonjs(),
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		commonjs(),
+
+    // In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
+
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('public'),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
     production && terser()
-  ]
+  ],
+  watch: {
+		clearScreen: false
+	}
 };
