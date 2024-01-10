464d855f79f807badb2105a6c24ab74853fd5a60
Brandon Ruffridge <bruffridge@users.noreply.github.com>
2024-01-10 17:45:07 -0500

    added submit command, and added text to speech

--- src/App.svelte
+++ src/App.svelte
@@ -124,8 +124,18 @@
       }}
       _insertKeyViewStyles={{displayCautionText: false}}
       demo={false}
+      textToSpeech={{
+        pitch: "1.4",
+        rate: "1.4"
+      }}
       speechToText={{
         webSpeech: "true",
+        commands: {
+          submit: "dude"
+        },
+        settings: {
+          substrings: "false"
+        },
         button: {
           default: {
             container: {
