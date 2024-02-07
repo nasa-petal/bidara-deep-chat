96ed3e3b81529523ba6702629d7d785c9519f46a
Jack Italiano <italiano.16@osu.edu>
2024-02-07 17:17:55 -0500

    ui changes

--- src/Chat.svelte
+++ src/Chat.svelte
@@ -1,25 +1,63 @@
 <script>
-	export let open = false;
-	export let thread;
-	export let handleClick = null;
-	export let handleDelete = null;
+    export let open = false;
+    export let thread;
+    export let handleClick = null;
+    export let handleDelete = null;
 
 </script>
 
-<div class="flex">
-	<button class="flex justify-between py-2 text-base font-sans block w-full focus:outline-none" class:open on:click={() => handleClick(thread)}>{thread.name} </button>
-	<button class="trash text-base font-sans bg-gray-400 p-2" on:click={() => handleDelete(thread)}>Delete</button>
+<div class="container flex items-center">
+    <button class="flex justify-between text-base font-sans block w-full focus:outline-none" class:open on:click={() => handleClick(thread)}>{thread.name} </button>
+    <a class="trash text-base font-sans bg-gray-400" on:click={() => handleDelete(thread)}>
+        <img class="trash-image unfocused" src="trash-can-red.svg" alt="delete"/>
+        <img class="trash-image focused" src="trash-can-white.svg" alt="delete"/>
+    </a>
 </div>
 
 <style>
-	button {
-    border-bottom: 1px solid rgb(209, 209, 214);
-	}
+    .container {
+        border-bottom: 1px solid rgb(209, 209, 214);
+        padding-top: 1em;
+        padding-bottom: 1em;
+        transition: background-color 0.3s ease;
+    }
 
-  .trash {
-    line-height: 1em;
-    border-radius: 1em;
-    background-color: rgb(242, 242, 247);
-  }
+    .unfocused {
+        opacity: 100%;
+        display: block;
+        transition: opacity 0.5s ease display ease 0.5s;
+    }
 
+    .focused {
+        opacity: 0%;
+        display: none;
+        transition: opacity 0.5s ease display ease 0.5s;
+    }
+
+    .trash-image {
+        width: 15px;
+        height: 15px;
+    }
+
+    .trash {
+        border-radius: 2em;
+        background-color: rgb(242, 242, 247);
+        padding: 0.5em;
+        transition: background-color 0.3s ease;
+    }
+
+    .trash:hover .unfocused{
+        opacity: 0%;
+        display: none;
+    }
+
+    .trash:hover .focused{
+        opacity: 100%;
+        display: block;
+    }
+
+    .trash:hover {
+        cursor: pointer;
+        background-color: rgb(255, 59, 48)
+    }
 </style>
