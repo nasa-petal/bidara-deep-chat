bb81142ee948a52ffb93e7c3ea000a5a69538f8d
Brandon Ruffridge <bruffridge@users.noreply.github.com>
2024-02-16 12:07:32 -0500

    fixed safari issues

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -7,19 +7,19 @@
     export let handleRename;
     
     let editing_name = false;
-    let editing_button;
+    let editing_input;
 
     function handleButtonClick() {
       editing_name = true;
     }
 
-    function handleButtonLeave() {
+    function handleInputLeave() {
       editing_name = false;
     }
 
-    async function handleButtonKeyDown(event) {
+    async function handleInputKeyDown(event) {
       if (event.key === 'Enter') {
-        const new_chat_name = event.target.innerText;
+        const new_chat_name = event.target.value;
 
         if (new_chat_name) {
           chat_name = new_chat_name;
@@ -28,7 +28,7 @@
 
         editing_name = false;
       } else if (event.key === 'Escape') { 
-        editing_button.blur()
+        editing_input.blur()
 
       } else if (event.key === ' '){
         event.preventDefault();
@@ -38,8 +38,8 @@
     }
 
     $: {
-      if (editing_name && editing_button) {
-        editing_button.focus();
+      if (editing_name && editing_input) {
+        editing_input.focus();
       }
     }
 
@@ -51,7 +51,7 @@
   </nav>
 
   {#if editing_name}
-    <button bind:this={editing_button} class="px-3 py-1 rounded-full" on:blur={handleButtonLeave} on:keydown={handleButtonKeyDown} contenteditable></button>
+    <input type="text" bind:this={editing_input} class="px-3 py-1 rounded-full" on:blur={handleInputLeave} on:keydown={handleInputKeyDown}/>
   {:else}
     <button class="focus:no-outline px-3 py-1 rounded-full" on:click={handleButtonClick}>{chat_name}</button>
   {/if}
@@ -63,17 +63,13 @@
     background-color: rgb(229, 229, 234);
     z-index: 10;
     border-bottom: 1px solid rgb(199, 199, 204);
+    max-width: 100vw;
   }
   button {
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
     background-color: rgb(229, 229, 234);
-    border: 2px solid rgb(229,229,234);
-  }
-  button:focus {
-    border: 2px solid rgb(199, 199, 204);
-    outline: rgb(199, 199, 204);
   }
 
 </style>
