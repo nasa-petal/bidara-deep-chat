d6df22b7edcc677ebfec9b879fb8e033699596ca
Jack Italiano <italiano.16@osu.edu>
2024-03-07 13:31:54 -0500

    ellipsis menu for survey

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,7 @@
 <script>
-    import Logo from './Logo.svelte'
     import Hamburger from './Hamburger.svelte'
+    import Menu from './Menu.svelte';
+    import MenuItem from './MenuItem.svelte';
 	
     export let sidebar = false
     export let chat_name;
@@ -58,13 +59,16 @@
   {:else}
     <button tabindex="0" class="focus:no-outline px-3 py-1 rounded-full" on:click={handleButtonClick}>{chat_name}</button>
   {/if}
-  <Logo/>	
+  <Menu>
+    <MenuItem><a class="w-full h-full p-1" tabindex="0" href="https://forms.gle/xDEixG5UJrFBwDKv5" target="_blank" rel="noopener">Send Feedback</a></MenuItem>
+    <MenuItem><a class="w-full h-full p-1" tabindex="0" href="https://github.com/nasa-petal" target="_blank" rel="noopener">Vist PeTaL</a></MenuItem>
+  </Menu>
 </header>
 
 <style>
   header {
     background-color: rgb(229, 229, 234);
-    z-index: 10;
+    z-index: 30;
     border-bottom: 1px solid rgb(180, 180, 180);
     height: 10em;
     margin-top: -7em;
