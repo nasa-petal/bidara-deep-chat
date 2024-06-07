b6613883f6975ffba7293076e0a70cdf5ee5593f
Jack Italiano <italiano.16@osu.edu>
2024-06-07 10:37:58 -0400

    extract bio SS search to own function in bidara
    - allows original SS search to be used by other assistants, or additional focused SS searches to be created

--- src/assistant/bidaraFunctions.js
+++ src/assistant/bidaraFunctions.js
@@ -1,9 +1,26 @@
 import { ssSearch, genImage, imageToText, getFileType, getImagePatterns } from "./commonFunctions";
 
+export async function bioSsSearch(params, context) {
+
+  const papers = await ssSearch(params, context);
+
+  const resMsg = `Verify the following results to be relevant to the question asked, and related to Biology.
+    If the results do not match these criteria, or if better results could be achieved through updated terms,
+    an additional search should be performed with updated terms.
+
+    <results> 
+    ${papers}
+    </results>
+
+    The results MUST be related to the question AND Biological in nature. Otherwise, you MUST search again without asking for confirmation.
+    `
+
+  return resMsg;
+}
 export async function callFunc(functionDetails, context) {
   let tmp = '';
   if(functionDetails.name == "get_graph_paper_relevance_search") {
-    tmp = await ssSearch(functionDetails.arguments);
+    tmp = await bioSsSearch(functionDetails.arguments);
   }
   else if(functionDetails.name == "text_to_image") {
     if (context?.processImageCallback && context?.lastMessageId) {
