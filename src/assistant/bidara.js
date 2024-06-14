0875d0326a5610d2f037af0996e36e3da4075eb9
Brandon Ruffridge <bruffridge@users.noreply.github.com>
2024-06-14 15:38:38 -0400

    bumped version no.

--- src/assistant/bidara.js
+++ src/assistant/bidara.js
@@ -5,7 +5,7 @@ const TEST_NAME = "BIDARA-TEST";
 
 export const NAME = PROD_NAME;
 
-export const VERSION = "1.8";
+export const VERSION = "1.9";
 
 export const LOGO = "bidara.png";
 export const LOGO_DESC = "girl with dark hair";
