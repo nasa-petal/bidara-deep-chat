f817e52b2d0925255bd19658bfff8e72f55fc436
saanaz379 <79679653+saanaz379@users.noreply.github.com>
2024-06-21 12:40:06 -0400

    Add reqManagerAsst to options

--- src/assistant/index.js
+++ src/assistant/index.js
@@ -1,5 +1,7 @@
 import * as bidaraAsst from './bidara'
+import * as reqManagerAsst from './reqManager'
 import { funcCalling as bidaraFuncCalling } from './bidaraFunctions'
+import { funcCalling as reqManagerFuncCalling } from './reqManagerFunctions'
 
 const bidara = {
 	name: bidaraAsst.NAME,
@@ -16,8 +18,24 @@ const bidara = {
 	customFunctions: bidaraAsst.FUNCTIONS.filter((func) => func.type === "function"),
 }
 
+const reqManager = {
+	name: reqManagerAsst.NAME,
+	config: reqManagerAsst.CONFIG,
+	history: reqManagerAsst.HISTORY,
+	funcCalling: reqManagerFuncCalling,
+	version: reqManagerAsst.VERSION,
+
+	tagline: reqManagerAsst.TAGLINE,
+	description: reqManagerAsst.TEXT_DESCRIPTION,
+	logo: reqManagerAsst.LOGO,
+	model: reqManagerAsst.MODEL,
+	builtInFunctions: reqManagerAsst.FUNCTIONS.filter((func) => func.type !== "function"),
+	customFunctions: reqManagerAsst.FUNCTIONS.filter((func) => func.type === "function"),
+}
+
 export const ASSISTANT_OPTIONS = [
 	bidara,
+	reqManager
 ]
 
 const testAssistant = ASSISTANT_OPTIONS.find((asst) => asst.name.includes("TEST"));
